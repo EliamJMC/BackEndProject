33165ff9ef041e226e3cf7164ca538ffaa0aa4d3
EliamJMC <jozadad2007@gmail.com>
2025-09-09 23:30:44 -0400

    11:30_Safety_Save

--- Assets/Character/Scripts/MainCharacterController.cs
+++ Assets/Character/Scripts/MainCharacterController.cs
@@ -1,3 +1,10 @@
+/*
+    [Header] es para crear un encabezado en el inspector de Unity.
+    [SerializeField] es para hacer que una variable privada sea visible y editable en el inspector de Unity.
+
+    private es para declarar una variable que solo puede ser accedida dentro de la misma clase.
+    public es para declarar una variable que puede ser accedida desde cualquier otra clase.
+*/
 using UnityEngine;
 
 public class MainCharacterController : MonoBehaviour
@@ -6,25 +13,36 @@ public class MainCharacterController : MonoBehaviour
     public const float Gravity = -9.81f;
 
     // Variables
-    public float Speed = 5f;
-    public float JumpForce = 7f;
+    [Header("Variables")]
+    public float speed = 5f;
+    public float jumpForce = 2f;
     public float rotationSpeed = 10f;
 
     // State
+    [Header("Estados")]
+    [SerializeField]
     private Vector3 velocity;
+    [SerializeField]
     private bool isGrounded;
 
     // Components
+    [Header("Componentes")]
+    [SerializeField]
     private CharacterController controller;
+    [SerializeField]
     private Animator animator;
+    private Transform camTransform;
 
     // *NOTA* En este void, se guardan todas las instrucciones que se ejecutan SOLO al iniciarse el programa.
     void Start()
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
+        camTransform = GetComponentInChildren<Camera>().transform;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
+    // *NOTA* En este void, se guardan todas las instrucciones que se ejecutan CADA FRAME.
     void Update()
     {
         isGrounded = controller.isGrounded;
@@ -53,7 +71,7 @@ public class MainCharacterController : MonoBehaviour
         if (move.magnitude >= 0.1f)
         {
             // Calcular la dirección del movimiento en función de la cámara
-            float targetAngle = Mathf.Atan2(moveNormalized.x, moveNormalized.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(moveNormalized.x, moveNormalized.z) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, 0.1f);
 
             // Rotar el personaje hacia la dirección del movimiento
@@ -61,13 +79,13 @@ public class MainCharacterController : MonoBehaviour
 
             // Mover el personaje
             Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-            controller.Move(moveDir.normalized * Speed * Time.deltaTime);
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
         // Salto
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(JumpForce * -2f * Gravity);
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * Gravity);
             animator.SetTrigger("Jump");
         }
 
